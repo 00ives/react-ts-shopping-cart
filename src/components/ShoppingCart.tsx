5c2c957de65480a94c923dde180eae2916508b2b
Ivor Lewis <101133399+00ives@users.noreply.github.com>
2023-08-24 18:28:02 -0600

    subtotal added to shopping cart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -7,6 +7,8 @@ import {
 } from 'react-bootstrap'
 import { useShoppingCart } from '../context/ShoppingCartContext'
 import { CartItem } from './CartItem'
+import { formatCurrency } from '../utilities/formatCurrency'
+import storeItems from '../data/items.json'
 
 type ShoppingCartProps = {
   isOpen: boolean
@@ -24,6 +26,15 @@ const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
           ))}
+          <div className='ms-auto fw-bold fs-5'>
+            Total{' '}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find((i) => i.id === cartItem.id)
+                return total + (item?.price || 0) * cartItem.quantity
+              }, 0)
+            )}
+          </div>
         </Stack>
       </OffcanvasBody>
     </Offcanvas>
